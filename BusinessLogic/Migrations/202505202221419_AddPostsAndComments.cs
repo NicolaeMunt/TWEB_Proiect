feb95fcf5e59c26c4d82d86de75a98aeef342f8b
Cazacu Nichita <nichiti.cazacu65@gmail.com>
2025-04-19 00:26:32 +0300

    Update site style

--- BusinessLogic/Migrations/202505202221419_AddPostsAndComments.cs
+++ BusinessLogic/Migrations/202505202221419_AddPostsAndComments.cs
@@ -1,8 +1,10 @@
-﻿namespace BusinessLogic.Migrations
+﻿using System.Data.Entity.Migrations;
+
+namespace BusinessLogic.Migrations
 {
     using System;
     using System.Data.Entity.Migrations;
-    
+
     public partial class AddPostsAndComments : DbMigration
     {
         public override void Up()
@@ -10,18 +12,18 @@
             CreateTable(
                 "dbo.Comment",
                 c => new
-                    {
-                        Id = c.Int(nullable: false, identity: true),
-                        PostId = c.Int(nullable: false),
-                        UserId = c.Int(nullable: false),
-                        ParentCommentId = c.Int(),
-                        Content = c.String(nullable: false),
-                        CreatedDate = c.DateTime(nullable: false),
-                        ModifiedDate = c.DateTime(),
-                        Upvotes = c.Int(nullable: false),
-                        Downvotes = c.Int(nullable: false),
-                        IsDeleted = c.Boolean(nullable: false),
-                    })
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    PostId = c.Int(nullable: false),
+                    UserId = c.Int(nullable: false),
+                    ParentCommentId = c.Int(),
+                    Content = c.String(nullable: false),
+                    CreatedDate = c.DateTime(nullable: false),
+                    ModifiedDate = c.DateTime(),
+                    Upvotes = c.Int(nullable: false),
+                    Downvotes = c.Int(nullable: false),
+                    IsDeleted = c.Boolean(nullable: false),
+                })
                 .PrimaryKey(t => t.Id)
                 .ForeignKey("dbo.Comment", t => t.ParentCommentId)
                 .ForeignKey("dbo.Post", t => t.PostId, cascadeDelete: true)
@@ -29,27 +31,27 @@
                 .Index(t => t.PostId)
                 .Index(t => t.UserId)
                 .Index(t => t.ParentCommentId);
-            
+
             CreateTable(
                 "dbo.Post",
                 c => new
-                    {
-                        Id = c.Int(nullable: false, identity: true),
-                        UserId = c.Int(nullable: false),
-                        Title = c.String(nullable: false, maxLength: 200),
-                        Content = c.String(nullable: false),
-                        CreatedDate = c.DateTime(nullable: false),
-                        ModifiedDate = c.DateTime(),
-                        Upvotes = c.Int(nullable: false),
-                        Downvotes = c.Int(nullable: false),
-                        IsDeleted = c.Boolean(nullable: false),
-                    })
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    UserId = c.Int(nullable: false),
+                    Title = c.String(nullable: false, maxLength: 200),
+                    Content = c.String(nullable: false),
+                    CreatedDate = c.DateTime(nullable: false),
+                    ModifiedDate = c.DateTime(),
+                    Upvotes = c.Int(nullable: false),
+                    Downvotes = c.Int(nullable: false),
+                    IsDeleted = c.Boolean(nullable: false),
+                })
                 .PrimaryKey(t => t.Id)
                 .ForeignKey("dbo.User", t => t.UserId)
                 .Index(t => t.UserId);
-            
+
         }
-        
+
         public override void Down()
         {
             DropForeignKey("dbo.Comment", "UserId", "dbo.User");
@@ -64,4 +66,71 @@
             DropTable("dbo.Comment");
         }
     }
+
 }
+
+/*
+public partial class AddPostsAndComments : DbMigration
+{
+    public override void Up()
+    {
+        CreateTable(
+            "dbo.Comment",
+            c => new
+            {
+                Id = c.Int(nullable: false, identity: true),
+                PostId = c.Int(nullable: false),
+                UserId = c.Int(nullable: false),
+                ParentCommentId = c.Int(),
+                Content = c.String(nullable: false),
+                CreatedDate = c.DateTime(nullable: false),
+                ModifiedDate = c.DateTime(),
+                Upvotes = c.Int(nullable: false),
+                Downvotes = c.Int(nullable: false),
+                IsDeleted = c.Boolean(nullable: false),
+            })
+            .PrimaryKey(t => t.Id)
+            .ForeignKey("dbo.Comment", t => t.ParentCommentId)
+            .ForeignKey("dbo.Post", t => t.PostId, cascadeDelete: true)
+            .ForeignKey("dbo.User", t => t.UserId)
+            .Index(t => t.PostId)
+            .Index(t => t.UserId)
+            .Index(t => t.ParentCommentId);
+
+        CreateTable(
+            "dbo.Post",
+            c => new
+            {
+                Id = c.Int(nullable: false, identity: true),
+                UserId = c.Int(nullable: false),
+                Title = c.String(nullable: false, maxLength: 200),
+                Content = c.String(nullable: false),
+                CreatedDate = c.DateTime(nullable: false),
+                ModifiedDate = c.DateTime(),
+                Upvotes = c.Int(nullable: false),
+                Downvotes = c.Int(nullable: false),
+                IsDeleted = c.Boolean(nullable: false),
+            })
+            .PrimaryKey(t => t.Id)
+            .ForeignKey("dbo.User", t => t.UserId)
+            .Index(t => t.UserId);
+
+    }
+
+    public override void Down()
+    {
+        DropForeignKey("dbo.Comment", "UserId", "dbo.User");
+        DropForeignKey("dbo.Comment", "PostId", "dbo.Post");
+        DropForeignKey("dbo.Post", "UserId", "dbo.User");
+        DropForeignKey("dbo.Comment", "ParentCommentId", "dbo.Comment");
+        DropIndex("dbo.Post", new[] { "UserId" });
+        DropIndex("dbo.Comment", new[] { "ParentCommentId" });
+        DropIndex("dbo.Comment", new[] { "UserId" });
+        DropIndex("dbo.Comment", new[] { "PostId" });
+        DropTable("dbo.Post");
+        DropTable("dbo.Comment");
+    }
+}
+
+}
+*/
