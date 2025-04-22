1341707d420427dd08dc3f88d060473beb4d71f1
Munteanu Nicolae <NickMunt2005@gmail.com>
2025-04-22 14:12:46 +0300

    Created first steps for implementing Business Logic, needs a liitle bit of rework on post creation

--- Domain/Enums/URole.cs
+++ Domain/Enums/URole.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Domain
+namespace Domain.Enums
 {
-    public class Class1
+    public enum URole
     {
+        User,
+        Moderator,
+        Admin
     }
 }
