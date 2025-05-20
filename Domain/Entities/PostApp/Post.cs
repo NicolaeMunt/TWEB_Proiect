ff3ed8232d5489f0c94ab69efa6b54c96eadced9
Cazacu Nichita <nichiti.cazacu65@gmail.com>
2025-05-21 00:47:19 +0300

    added class of post and comments

---
+++ Domain/Entities/PostApp/Post.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities.User;
+using System;
+using System.Collections.Generic;
+
+namespace Domain.Entities.Post
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public int Upvotes { get; set; }
+        public int Downvotes { get; set; }
+        public bool IsDeleted { get; set; }
+
+        // Navigation properties
+        public virtual Domain.Entities.User.User User { get; set; }
+        public virtual ICollection<Comment> Comments { get; set; } = new List<Comment>();
+    }
+}
---
+++ Domain/Interfaces/IPostService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities.Post;
+
+namespace Domain.Interfaces
+{
+    public interface IPostService
+    {
+        Post GetPostById(int id);
+        IEnumerable<Post> GetPosts(int page = 1, int pageSize = 10);
+        IEnumerable<Post> GetPostsByUserId(int userId);
+        int CreatePost(Post post);
+        bool UpdatePost(Post post, int currentUserId);
+        bool DeletePost(int id, int currentUserId);
+
+        Comment GetCommentById(int id);
+        IEnumerable<Comment> GetCommentsByPostId(int postId);
+        int CreateComment(Comment comment);
+        bool UpdateComment(Comment comment, int currentUserId);
+        bool DeleteComment(int id, int currentUserId);
+    }
+}
